b937885073913ec2fa3dabf3735d939429027b09
TodeschiniDev <rodrigotdev@gmail.com>
2025-08-22 21:01:56 +0100

    feat: basic acceleration and break implementation

--- Assets/Code/Scripts/PlayerMovement.cs
+++ Assets/Code/Scripts/PlayerMovement.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody player_rb;
+
+    [SerializeField] float accelerationForce = 10f;
+    [SerializeField] float brakeForce = 10f;
+    private float accelerationInputValue;
+    private float brakeInputValue;
+
+    public void AccelerationInput(InputAction.CallbackContext context)
     {
-        
+        accelerationInputValue = context.ReadValue<float>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BrakeInput(InputAction.CallbackContext context)
     {
-        
+        brakeInputValue = context.ReadValue<float>();
+    }
+    void Start()
+    {
+        player_rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Accelerate();
+        Brake();
+    }
+
+    void Accelerate()
+    {
+        Vector3 acceleration = new Vector3(accelerationInputValue, 0, 0) * accelerationForce;
+        player_rb.AddForce(acceleration, ForceMode.Force);
+    }
+
+    void Brake()
+    {
+        Vector3 brake = new Vector3(-brakeInputValue, 0, 0) * brakeForce;
+        player_rb.AddForce(brake, ForceMode.Force);
     }
 }
